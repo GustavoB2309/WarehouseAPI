371de5021b34493eb82f6dd6f4a9fbb462483934
Gustavo <tomazinigustavo2@gmail.com>
2026-08-26 16:16:49 -0400

    Desenvolvendo a base banco de dados.

---
+++ WarehouseAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WarehouseAPI.Models;
+
+namespace WarehouseAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+    public DbSet<Produto> Produtos { get; set; }
+}
+}
--- WarehouseAPI/Program.cs
+++ WarehouseAPI/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using WarehouseAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Warehouse API ligada.");
+using (var scope = app.Services.CreateScope())
+{
+    var banco = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    banco.Database.EnsureCreated();
+}
+
+app.MapGet("/", () => "A Warehouse API está rodando.");
 
 app.Run();
