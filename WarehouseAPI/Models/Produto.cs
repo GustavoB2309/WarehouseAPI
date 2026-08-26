3d286a05c590994b01de098517b03cd556307488
Gustavo <tomazinigustavo2@gmail.com>
2026-08-26 15:52:57 -0400

    Banco de dados e o que terá nos produtos do banco de dados adicionado.

---
+++ WarehouseAPI/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WarehouseAPI.Models
+{
+    [Table("Produtos")]
+
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CodigoDebarras { get; set; }
+        public double Preco { get; set; }
+        public int QuantidadeEmEstoque { get; set; }
+    }
+}
