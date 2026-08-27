a052785670f1a1399875613ba2ff29408639e631
Gustavo <tomazinigustavo2@gmail.com>
2026-08-27 15:02:25 -0400

    Swagger UI implementado.

--- WarehouseAPI/Program.cs
+++ WarehouseAPI/Program.cs
@@ -8,12 +8,21 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(connectionString));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
     var banco = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     banco.Database.EnsureCreated();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.MapGet("/", () => "A Warehouse API está rodando.");
