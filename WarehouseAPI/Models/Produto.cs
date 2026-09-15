c76764ff52d5e01d492c8287fe6da3bb77f8d85c
Gustavo <tomazinigustavo2@gmail.com>
2026-09-15 14:02:31 -0400

    Implementada blindagem cadastral na entidade produto via data annotations.

--- WarehouseAPI/Models/Produto.cs
+++ WarehouseAPI/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace WarehouseAPI.Models
 {
@@ -8,8 +9,11 @@ namespace WarehouseAPI.Models
     public class Produto
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório.")]
         public string Nome { get; set; }
+        [StringLength(20, ErrorMessage = "O código de barras não pode ter mais de 20 caracteres.")]
         public string CodigoDebarras { get; set; }
+        [Range(0.01, 999999.99, ErrorMessage = "O valor deve ser maior que zero.")]
         public decimal Preco { get; set; }
         public decimal PrecoCusto { get; set; }
         public int QuantidadeEmEstoque { get; set; }
